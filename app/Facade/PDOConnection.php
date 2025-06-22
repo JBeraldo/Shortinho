17a0e1c9ae19831d34e51395e9710ece56cfa384
JBeraldo <joaoangelo409@gmail.com>
2025-06-22 02:29:06 -0300

    FIX: Corrigido conexão PDO para não limitar ao driver do postrgres

--- app/Facade/PDOConnection.php
+++ app/Facade/PDOConnection.php
@@ -11,12 +11,7 @@ class PDOConnection {
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            
-            $database_name = $_ENV['DB_DATABASE'];
-            $database_user = $_ENV['DB_USERNAME'];
-            $database_password = $_ENV['DB_PASSWORD'];
-
-            self::$instance = new PDO("pgsql:host=db;port=5432;dbname=$database_name;user=$database_user;password=$database_password",);
+            self::$instance = new PDO($_ENV['DATABASE_DSN']);
             self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
 
